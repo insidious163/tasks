43d273c213d63d824c24ef583ce61c9171b0df94
aleksandr.vyguliarnyi <ce-aleksandr.vyguliarnyi@regnology.net>
2023-06-03 19:23:15 +0200

    Task added

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import Task from "./task/Task";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Task />
   </React.StrictMode>
 );
